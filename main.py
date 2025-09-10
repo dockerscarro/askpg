c8469b892e31140653ce03dfd0d3bf064025b70a
dockerscarro <dockerscarro@gmail.com>
2025-09-10 13:59:07 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+# main.py
+def add_numbers(a, b):
+    """Adds two numbers"""
+    return a + b
+
+def multiply_numbers(a, b):
+    """Multiplies two numbers"""
+    return a * b
+
+def divide_numbers(a, b):
+    """Divides a by b"""
+    return a / b
+
+print(add_numbers(2, 3))
+print(multiply_numbers(4, 5))
+print(divide_numbers(10, 2))
