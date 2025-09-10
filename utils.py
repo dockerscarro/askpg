f3265bd105d53a6add3f1976d5e9f80f233a28e7
dockerscarro <dockerscarro@gmail.com>
2025-09-10 13:20:54 +0530

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,10 @@
+def calculate_discounted_total(cart, discount=0):
+        """
+        Calculates total price after discount.
+        BUG: Ignores quantity of each item.
+        """
+        total = 0
+        for item in cart:
+            total += item["price"]  # ❌ Bug: should multiply by qty
+        total -= discount
+        return total
